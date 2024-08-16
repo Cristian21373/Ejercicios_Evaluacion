b26ccbf32665003202a3c167e62556cc7acc95ce
Cristian Bahamon <Cbahamongranados@gmail.com>
2024-08-16 16:46:09 -0500

    Se sube el ejercicio con entrada de datos y el otro cumpliendo con los puntos del del examen

---
+++ Ejercicio_Examen/Ejercicio/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Threading.Tasks;
+using static Ejercicio.Program;
+
+
+namespace Ejercicio
+{
+    internal class Program
+    {
+        public class persona
+        {
+            public String nombre { get; set; }
+            public int edad { get; set; }
+            public String direccion { get; set; }
+
+            //Constructor sin parametros
+            public persona()
+            {
+                nombre = "yordy";
+                edad = 19;
+                direccion = "campo";
+
+            }
+
+            //Constructor con parametros
+            public persona(String nombre, int edad, String Direccion)
+            {
+                this.nombre = nombre;
+                this.edad = edad;
+                this.direccion = Direccion;
+            }
+
+            //Metodo para mostrar datos de la persona
+            public virtual void MostrarInformacion()
+            {
+                Console.WriteLine($"Nombre: {nombre}");
+                Console.WriteLine($"Edad: {edad}");
+                Console.WriteLine($"Direccion: {direccion}");
+            }
+
+            public async Task LeerDatosAsync()
+            {
+                Console.WriteLine("Obteniendo datos de la persona");
+                await Task.Delay(5000); //Retraso de 5 segundos 
+                MostrarInformacion();
+            }
+
+        }
+
+
+
+
+
+        public class Empleado : persona, ICalculable
+        {
+            public int Salario { get; set; }
+            public int DiasTrabajados { get; set; }
+
+            public Empleado(string nombre, int edad, string direccion, int salario, int diasTrabajados)
+                : base(nombre, edad, direccion)
+            {
+                this.Salario = salario;
+                this.DiasTrabajados = diasTrabajados;
+            }
+
+
+
+
+
+
+            public double CalcularSalario()
+            {
+
+                double operacion = Salario / 30;
+                return operacion * DiasTrabajados;
+
+            }
+
+
+
+
+
+
+            public override void MostrarInformacion()
+            {
+                base.MostrarInformacion();
+                Console.WriteLine($"Salario: {Salario}");
+                Console.WriteLine($"Dias trabajados: {DiasTrabajados}");
+                Console.WriteLine($"Total: {CalcularSalario()}");
+            }
+
+            public async Task LeerDatosAsync()
+            {
+                Console.WriteLine("Leyendo datos del empleado");
+                await Task.Delay(5000); //Retraso de 5 segundos 
+                MostrarInformacion();
+            }
+        }
+
+
+        //    Muestra datos normal sin async y await
+
+        //static void Main(string[] args)
+        //{
+        //    persona persona = new persona();
+        //    persona.MostrarInformacion();
+
+        //    Console.WriteLine();
+
+        //    persona = new persona();
+        //    persona persona2 = new persona("ana", 20, "Campoalegre");
+        //    persona2.MostrarInformacion();
+
+        //    Console.WriteLine();
+
+
+        //    Empleado empleado = new Empleado("Cristian", 18, "Bogota", 1300000, 6);
+        //    empleado.MostrarInformacion();
+
+
+        //}
+
+
+
+
+
+        //Muestra datos con async y await
+        static async Task Main(string[] args)
+        {
+            persona persona1 = new persona();
+            await persona1.LeerDatosAsync();
+
+            Console.WriteLine();
+
+            persona persona2 = new persona("ana", 20, "Campoalegre");
+            await persona2.LeerDatosAsync();
+
+            Console.WriteLine();
+
+            Empleado empleado = new Empleado("Cristian", 18, "Bogota", 1300000, 6);
+            await empleado.LeerDatosAsync();
+        }
+    }
+}
---
+++ ejercicio con entrada de datos/Ejercicio_entrada_de_datos/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Ejercicio_con_entrada_de_datos
+{
+    internal class Program
+    {
+        public class Persona
+        {
+            public string Nombre { get; set; }
+            public int Edad { get; set; }
+            public string Direccion { get; set; }
+            public int Salario { get; set; }
+            public int DiasTrabajados { get; set; }
+            public string Telefono { get; set; }
+
+            // Constructor con entrada de datos por el usuario
+            public Persona()
+            {
+                Console.Write("Ingrese el nombre: ");
+                Nombre = Console.ReadLine();
+
+                Console.Write("Ingrese la edad: ");
+                Edad = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Ingrese la direccion: ");
+                Direccion = Console.ReadLine();
+
+                Console.Write("Ingrese el telefono: ");
+                Telefono = Console.ReadLine();
+
+                Console.Write("Ingrese el salario: ");
+                Salario = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Ingrese los días trabajados: ");
+                DiasTrabajados = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Metodo para calcular el salario
+            public double CalcularSalario()
+            {
+                double operacion = Salario / 30;
+                return operacion * DiasTrabajados;
+            }
+
+            // Metodo para mostrar datos de la persona
+            public void MostrarInformacion()
+            {
+                Console.WriteLine("--------------------");
+                Console.WriteLine("| Datos obtenidos:  |");
+                Console.WriteLine("--------------------");
+                Console.WriteLine($"Nombre: {Nombre}");
+                Console.WriteLine($"Edad: {Edad}");
+                Console.WriteLine($"Dirección: {Direccion}");
+                Console.WriteLine($"Teléfono: {Telefono}");
+                Console.WriteLine($"Salario: {Salario}");
+                Console.WriteLine($"Días trabajados: {DiasTrabajados}");
+                Console.WriteLine($"Total salario calculado: {CalcularSalario()}");
+            }
+
+            public async Task LeerDatosAsync()
+            {
+                Console.WriteLine("");
+                Console.WriteLine("-------------------------------------------------------");
+                Console.WriteLine("| Obteniendo datos de la persona. Por favor espere...  |");
+                Console.WriteLine("-------------------------------------------------------");
+                await Task.Delay(5000); // Retraso de 5 segundos 
+                MostrarInformacion();
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Crear una persona");
+            Persona persona1 = new Persona();
+            await persona1.LeerDatosAsync();
+        }
+    }
+}
